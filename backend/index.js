e8bdcdd3d4b10f35c076ce2b94ec0b5991bb983c
ambili-nv <ambilinadvilvalappil@gmail.com>
2025-03-20 02:23:55 +0530

    some updations

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: '*',
+    origin: '/https://mcq-based-test.vercel.app',
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
 }));
